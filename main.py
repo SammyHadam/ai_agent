4c3ae104a3d443543d7828226795e5cc76056514
SammyHadam <sammyhadams@gmail.com>
2025-09-11 07:52:18 -0500

    added feature to accept user input from sys.arg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -8,8 +10,18 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    args = sys.argv[1:]
+
+    if not args:
+        print("Error: Prompt not provided. Please try again.")
+        print(f"Usage: uv python {sys.argv[0]} <your prompt here>")
+        sys.exit(1)
+
+    prompt = " ".join(args)
+    print(prompt + "\n" + len(prompt) * "-" + "\n")
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        model='gemini-2.0-flash-001', contents=prompt)
 
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
