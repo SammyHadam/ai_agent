b4bb92c24b1847d0bac76f8ee9e957030d551585
SammyHadam <sammyhadams@gmail.com>
2025-09-13 19:22:21 -0500

    added support for verbose flag

--- main.py
+++ main.py
@@ -10,22 +10,45 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("Error: Prompt not provided. Please try again.")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print(f"Usage: uv python {sys.argv[0]} <your prompt here>")
         sys.exit(1)
 
     prompt = " ".join(args)
     print(prompt + "\n" + len(prompt) * "-" + "\n")
 
+    if verbose:
+        print(f"User prompt: {prompt}")
+    
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+    
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt)
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
 
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+    if verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
     print("Response:")
     print(response.text)
 
